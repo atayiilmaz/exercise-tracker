63a722a66cb1da33dc93e378c38c52662f548499
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-14 10:48:48 +0300

    added new routes

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ const exerciseSchema = mongoose.Schema({
   username: String,
   description: String,
   duration: Number,
-  date: String,
+  date: Date,
   userId: String
 
+}, {
+  versionKey: false
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
@@ -110,7 +112,50 @@ app.post('/api/users/:_id/exercises', async (req ,res) => {
 
 app.get('/api/users/:_id/logs', async(req, res) => {
 
+  let {from, to, limit} = req.query;
+  const userId = req.params._id;
+  const foundUser = await User.findById(userId);
+
+  if(!foundUser) {
+    res.json({ message: 'No user exists' });
+  }
+
+  let filter = { userId };
+  let dateFilter = {};
+
+  if(from) {
+
+    dateFilter['$gte'] = new Date(from);
+  }
+  if(to) {
+    dateFilter['$lte'] = new Date(to);
+  }
+  if(from || to) {
+    filter.date = dateFilter;
+  }
+
+  if(!limit) {
+    limit = 100;
+  }
+
+  let exercises = await Exercise.find(filter).limit(limit);
   
+  exercises.map((exercise) => {
+    
+    return {
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString()
+    }
+  });
+  
+  res.json({
+    username: foundUser.username,
+    count: exercises.length,
+    _id: userId,
+    log: exercises
+
+  })
 
 });
 
